060587e48c550107b91ba2bb0be659da2c2d18b6
Andrew Spencer <andrewos@bu.edu>
2020-12-04 10:51:20 -0700

    set up

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv')
 
 const app = express()
 
@@ -9,8 +10,8 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -21,3 +22,8 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+dotenv.config()
+
+console.log(`Your API key is ${process.env.API_KEY}`)
+console.log('YOOOOOOOOOOOOOOOOOO')
